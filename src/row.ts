23bd3da97448f4c38605dfb86374f9f50c733725
Peter Stromquist <peter@twistedstream.com>
2024-02-05 09:12:02 -0600

    fix: ensure columns exist for each row property

--- src/row.ts
+++ src/row.ts
@@ -6,32 +6,40 @@ import { Row, RowData } from "./types";
 export function valuesToRow(
   rowValues: any[],
   columns: string[],
-  rowNumber: number,
+  rowNumber: number
 ): Row {
   return rowValues.reduce(
     (row, value, index) => {
       row[columns[index]] = value;
       return row;
     },
-    { _rowNumber: rowNumber },
+    { _rowNumber: rowNumber }
   );
 }
 
 export function rowToValues(row: RowData, columns: string[]): any[] {
+  // ensure columns exist for each row property
+  const missingColumns = Object.keys(row).filter((p) => !columns.includes(p));
+  if (missingColumns.length > 0) {
+    throw new Error(
+      `Table columns missing that exist as row properties: ${missingColumns.join(", ")}`
+    );
+  }
+
   return columns.map((c) => row[c]);
 }
 
 export function processUpdatedData(
   updatedValueRange: sheets_v4.Schema$ValueRange,
   sheetName: string,
-  submittedRowValues: any[],
+  submittedRowValues: any[]
 ): { updatedRowValues: any[]; updatedRowNumber: number } {
   const { range: _range, values: _values } = updatedValueRange;
   const range = assertValue(_range, "Updated value range has empty range");
   const values = assertValue(_values, "Updated value range has empty values");
   if (values.length !== 1) {
     throw new Error(
-      `Expected one row of values, but instead got ${values.length}`,
+      `Expected one row of values, but instead got ${values.length}`
     );
   }
   const updatedRowValues = values[0];
@@ -52,19 +60,19 @@ export function processUpdatedData(
   if (matches.some((m) => m !== true)) {
     throw new ErrorWithData(
       "One or more updated row values don't match corresponding submitted values",
-      matches,
+      matches
     );
   }
 
   const { sheet, startRow, endRow } = parseRange(range);
   if (sheetName !== sheet) {
     throw new Error(
-      `Updated range sheet name '${sheet}' doesn't match submitted sheet name '${sheetName}'`,
+      `Updated range sheet name '${sheet}' doesn't match submitted sheet name '${sheetName}'`
     );
   }
   if (startRow !== endRow) {
     throw new Error(
-      `Updated range start row (${startRow}) doesn't match end row (${endRow})`,
+      `Updated range start row (${startRow}) doesn't match end row (${endRow})`
     );
   }
 
