a35e7af21a75cad1dee8c7be7f830b30ca0cd066
Peter Stromquist <peter@twistedstream.com>
2024-12-03 12:02:48 -0600

    refactor: upgrade remaining dependencies (#44)
    * refactor: upgrade @googleapis/sheets -> v9
    * refactor: upgrade async-mutex -> 0.5
    * refactor: upgrade google-auth-library -> 9.15
    * refactor: upgrade lodash
      - same version, but update did change package-lock
    * refactor: upgrade dotenv -> 16.4.7
    * refactor: upgrade fix-tsup-cjs
      - same version, but update did change package-lock
    * refactor: upgrade nodemon -> 3.1.7
    * refactor: upgrade prettier -> 3.4.1
      - unify CLI and IDE output using .prettierrc
      - one-time modification of all files that need format updates
      - enhance gh build script to call format:check script, which fails if any formatting is required
    * refactor: upgrade sinon -> 19.0.2
    * refactor: upgrade tsup -> 8.3.5

--- src/concurrency.ts
+++ src/concurrency.ts
@@ -4,7 +4,7 @@ const mutexes = new Map<string, Mutex>();
 
 export async function lock<T>(
   spreadsheetId: string,
-  worker: () => T
+  worker: () => T,
 ): Promise<T> {
   const mutex = mutexes.get(spreadsheetId) ?? new Mutex();
   mutexes.set(spreadsheetId, mutex);
